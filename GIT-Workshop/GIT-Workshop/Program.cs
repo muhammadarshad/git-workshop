9c9904ba5d2ac6a010f23ba1ecb09eb84567a8eb
AAW <adeelahhmed.warraich@gmail.com>
2018-03-30 19:57:27 +0500

    1st commit

--- GIT-Workshop/GIT-Workshop/Program.cs
+++ GIT-Workshop/GIT-Workshop/Program.cs
@@ -10,6 +10,8 @@ namespace GIT_Workshop
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
